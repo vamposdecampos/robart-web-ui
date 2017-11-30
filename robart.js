ce564b8983df90f71e776ee7a11f767ebef0ebef
Alex Badea <vamposdecampos@gmail.com>
2017-11-29 20:06:48 -0800

    js: add Hammer custom event handler for svg-pan-zoom
    Straight from the docs.  This enables pinch-zoom on Android (at least).

--- robart.js
+++ robart.js
@@ -27,7 +27,51 @@ RobartView.prototype = {
 			return;
 		}
 
+		var eventsHandler = {
+			haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel'],
+			init: function(options) {
+				var instance = options.instance,
+					initialScale = 1,
+					pannedX = 0,
+					pannedY = 0;
+
+				this.hammer = Hammer(options.svgElement, {
+					inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput
+				});
+				this.hammer.get('pinch').set({enable: true})
+				this.hammer.on('doubletap', function(ev) {
+					instance.zoomIn();
+				});
+				this.hammer.on('panstart panmove', function(ev) {
+					if (ev.type === 'panstart') {
+						pannedX = 0;
+						pannedY = 0;
+					}
+
+					instance.panBy({x: ev.deltaX - pannedX, y: ev.deltaY - pannedY});
+					pannedX = ev.deltaX;
+					pannedY = ev.deltaY;
+				});
+
+				this.hammer.on('pinchstart pinchmove', function(ev) {
+					if (ev.type === 'pinchstart') {
+						initialScale = instance.getZoom();
+						instance.zoom(initialScale * ev.scale);
+					}
+					instance.zoom(initialScale * ev.scale);
+				});
+
+				// Prevent moving the page on some devices when panning over SVG
+				options.svgElement.addEventListener('touchmove', function(e) { e.preventDefault(); });
+			},
+
+			destroy: function() {
+				this.hammer.destroy();
+			}
+		};
+
 		this.zoomer = svgPanZoom('#' + this.div_id + ' > svg', {
+			customEventsHandler: eventsHandler,
 			panEnabled: true,
 			controlIconsEnabled: false,
 			zoomEnabled: true,
